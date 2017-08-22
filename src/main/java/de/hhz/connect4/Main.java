3b23393ff43c1f2912bcc06961dee77518d1117a
schmolli <martin.schmollinger@gmail.com>
2017-08-22 16:06:51 +0200

    Added initial class for starting the JavaFX app

---
+++ src/main/java/de/hhz/connect4/Main.java
@@ -0,0 +1,21 @@
+package de.hhz.connect4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Martin Schmollinger
+ *
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("Hello!");
+    }
+
+}
